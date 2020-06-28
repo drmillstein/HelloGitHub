3d87dab67d427fb1f22fbe4744646c71891167d9
drmillstein <drmillstein@gmail.com>
2020-06-28 13:23:11 -0700

    Added default to variable name.  Arg is now optional.

--- HelloGit.java
+++ HelloGit.java
@@ -2,7 +2,10 @@ public class HelloGit
 {
     public static void main(String[] args)
     {
-        String name = args[0];
+        String name = "Dennis";
+
+        if (args.length > 0)
+            name = args[0];
         System.out.printf("Hello %s\n", name);
     }
 }
